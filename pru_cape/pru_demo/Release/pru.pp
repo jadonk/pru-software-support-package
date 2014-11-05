929c4b05f2e042ba0c43bc91f8a8dba1586e6c9c
mwatkins <m-watkins@ti.com>
2014-11-05 15:10:47 -0800

    Add PRU labs and demos
    * Labs include:
       - Lab 1   Toggle LED with PRU GPO
       - Lab 2   Read Push Button Switch on PRU0 GPI & Toggle LED with PRU1 GPO
    * Demos include:
       - LED
       - Push Button Switch
       - Audio
       - UART
    * Update Software Manifest
    * Add ReadMe file

---
+++ pru_cape/pru_demo/Release/pru.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+pru.obj: ../pru.c
+pru.obj: C:/TI/AM335X_StarterWare_02_00_01_01/include/hw/soc_AM335x.h
+pru.obj: ../pru.h
+pru.obj: ../hw_pruss.h
+pru.obj: ../hw_pru_ctrl.h
+pru.obj: C:/TI/AM335X_StarterWare_02_00_01_01/include/hw/hw_cm_per.h
+pru.obj: C:/TI/AM335X_StarterWare_02_00_01_01/include/hw/hw_types.h
+pru.obj: C:/TI/AM335X_StarterWare_02_00_01_01/include/delay.h
+
+../pru.c: 
+C:/TI/AM335X_StarterWare_02_00_01_01/include/hw/soc_AM335x.h: 
+../pru.h: 
+../hw_pruss.h: 
+../hw_pru_ctrl.h: 
+C:/TI/AM335X_StarterWare_02_00_01_01/include/hw/hw_cm_per.h: 
+C:/TI/AM335X_StarterWare_02_00_01_01/include/hw/hw_types.h: 
+C:/TI/AM335X_StarterWare_02_00_01_01/include/delay.h: 
---
+++ pru_cape/pru_demo/Release/subdir_vars.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../pru_cape_demo.cmd 
+
+OBJ_SRCS += \
+T:/workdir/pru/pru_cape/pru_fw/PRU_Audio/Debug/PRU_Audio_image.obj \
+T:/workdir/pru/pru_cape/pru_fw/PRU_Hardware_UART/Debug/PRU_Hardware_UART_image.obj \
+T:/workdir/pru/pru_cape/pru_fw/PRU_LED0/Debug/PRU_LED0_image.obj \
+T:/workdir/pru/pru_cape/pru_fw/PRU_LED1/Debug/PRU_LED1_image.obj \
+T:/workdir/pru/pru_cape/pru_fw/PRU_Switch/Debug/PRU_Switch_image.obj 
+
+C_SRCS += \
+../pru.c \
+../pru_cape_demo.c 
+
+OBJS += \
+./pru.obj \
+./pru_cape_demo.obj 
+
+C_DEPS += \
+./pru.pp \
+./pru_cape_demo.pp 
+
+C_DEPS__QUOTED += \
+"pru.pp" \
+"pru_cape_demo.pp" 
+
+OBJS__QUOTED += \
+"pru.obj" \
+"pru_cape_demo.obj" 
+
+C_SRCS__QUOTED += \
+"../pru.c" \
+"../pru_cape_demo.c" 
+
+
